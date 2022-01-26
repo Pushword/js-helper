99a0e9c6947217e2090b14e0f1b181b1b7663f84
RobinDev <contact@piedweb.com>
2022-01-26 13:24:20 +0000

    update pushword/js-helper version

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require("fslightbox");
+import {
+    uncloakLinks,
+    readableEmail,
+    convertImageLinkToWebPLink,
+    replaceOn,
+    liveBlock,
+} from "@pushword/js-helper/src/helpers.js";
+import { allClickable } from "@pushword/js-helper/src/clickable.js";
+
+function onDomChanged() {
+    liveBlock();
+    convertImageLinkToWebPLink();
+    uncloakLinks();
+    readableEmail(".cea");
+    replaceOn();
+    refreshFsLightbox();
+    allClickable(".clickable");
+}
+
+function onPageLoaded() {
+    onDomChanged();
+    new FsLightbox();
+}
+
+document.addEventListener("DOMContentLoaded", onPageLoaded());
+document.addEventListener("DOMChanged", onDomChanged);
